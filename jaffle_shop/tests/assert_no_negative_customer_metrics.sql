3573e00f2c0d869fd6f886d60dfb2ecfd17bf8d5
Angel Rodriguez Chicote <angel.rodriguez@sdggroup.com>
2025-10-27 08:58:00 +0100

    version 1

--- jaffle_shop/tests/assert_no_negative_customer_metrics.sql
+++ jaffle_shop/tests/assert_no_negative_customer_metrics.sql
@@ -2,14 +2,8 @@
 -- Todas las métricas financieras y de conteo deben ser >= 0
 
 select
-    customer_id,
-    count_lifetime_orders,
-    lifetime_spend_pretax,
-    lifetime_tax_paid,
-    lifetime_spend
+    customer_id
 from {{ ref('marts__customers') }}
 where
-    count_lifetime_orders < 0
-    or lifetime_spend_pretax < 0
-    or lifetime_tax_paid < 0
-    or lifetime_spend < 0
+    lifetime_value < 0
+    
